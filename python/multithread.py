30894b55963abc642d8d6e428cbe3e09917e27ac
Eduard Zelenay <eduardzelenay23@gmail.com>
2023-12-22 20:38:22 +0100

    changed python script to respond to GET and POST request from app

--- python/multithread.py
+++ python/multithread.py
@@ -32,30 +32,26 @@ class PortParser:
             while self.lock:
                 pass
             
-            json_bytes = self.ser.read(60)
+            json_bytes = self.ser.read(self.ser.inWaiting())
             buffer += json_bytes
 
-            if b'{' in buffer and b'}' in buffer:
-                json_start = buffer.index(b'{')
-                json_end = buffer.index(b'}') + 1
-                json_obj = buffer[json_start:json_end]
-                json_input = json_obj.decode('utf-8')
-                data = parse_json_data(json_input)
+            data=to_jason(buffer)
 
-                if data is not None:
-                    #if autodesk_window_in_focus:
-                    focus_autodesk_window()
-                    perform_actions(data)
-                        
+            if data is not None:
+                #if autodesk_window_in_focus:
+                focus_autodesk_window()
+                perform_actions(data)
+                json_end = buffer.index(b'}') + 1        
                 print(data)
                 buffer = buffer[json_end:]
     
-    def addPayload(self, payload)-> bytearray:
+    def addPayload(self, payload,response=True)-> bytearray:
         self.lock = True
         self.payload = payload
         
         self.ser.write(payload)
-        self.response = self.ser.readline()
+        if response:
+            self.response = self.ser.readline()
         self.lock = False
         return self.response
         
@@ -114,26 +110,60 @@ def focus_autodesk_window():
 
 # Define a custom handler to serve JSON data
 class MyHandler(http.server.BaseHTTPRequestHandler):
+    global portParser 
     def do_GET(self):
-        if self.path == '/json_data':
+        if self.path == '/dpi':
+            send_data = "$"
+            dpi_data=portParser.addPayload(send_data.encode('utf-8'))
+            print(dpi_data)
+            dpi_data=to_jason(dpi_data)
+            if dpi_data is None:
+                self.send_response(400)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Invalid data format')
+                return
+
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            
-            json_data = {
-                'Rotate_x': 30,
-                'Rotate_y': 45,
-                'Rotate_z': 0,
-                'Button': 'none',
-                'Zoom': -10
-            }
-
+            json_data = dpi_data
             json_str = json.dumps(json_data)
             self.wfile.write(json_str.encode('utf-8'))
         else:
             self.send_response(404)
             self.end_headers()
             self.wfile.write(b'Not Found')
+
+    def do_POST(self):
+        if self.path == '/dpi_write':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            parsed_data = parse_json_data(post_data)
+            dpi_value = parsed_data.get('dpi')
+            button0_value = parsed_data.get('button0')
+            button1_value = parsed_data.get('button1')
+
+            if not all(isinstance(value,int) and value >= 0 for value in [dpi_value, button0_value, button1_value]):
+                # Respond with a 400 Bad Request status if any value is not a digit
+                self.send_response(400)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Invalid data format')
+                return
+            
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Success')
+            send_data=str(dpi_value)+','+str(button0_value)+','+str(button1_value) 
+            portParser.addPayload(send_data.encode('utf-8'),response=False)
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not Found')           
 
 def start_http_server():
     host = 'localhost'
@@ -145,6 +175,17 @@ def start_http_server():
             httpd.serve_forever()
         except KeyboardInterrupt:
             pass
+
+def to_jason(data):
+    if b'{' in data and b'}' in data:
+                json_start = data.index(b'{')
+                json_end = data.index(b'}') + 1
+                json_obj = data[json_start:json_end]
+                json_input = json_obj.decode('utf-8')
+                return_data = parse_json_data(json_input)
+                return return_data
+    else:
+        return None
 
 def parse_json_data(json_input):
     try:
@@ -201,8 +242,8 @@ if __name__ == "__main__":
     main_thread.start()
 
     # Create a thread to run the HTTP server concurrently
-    # server_thread = threading.Thread(target=start_http_server)
-    # server_thread.start()
+    server_thread = threading.Thread(target=start_http_server)
+    server_thread.start()
 
     # Run the focus_autodesk_window() function in the main thread
     # while True:
